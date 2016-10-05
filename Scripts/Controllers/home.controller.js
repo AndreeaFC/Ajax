e998233de807c70daa632f4b7fc13fbbb7643919
AndreeaFC <andreea_vhr@yahoo.com>
2016-10-05 17:35:52 +0200

    basic

--- Scripts/Controllers/home.controller.js
+++ Scripts/Controllers/home.controller.js
@@ -3,7 +3,17 @@
 angular.module("mainModule")
     .controller("HomeController", [
         "$scope",
-        function ($scope) {
+        "$http",
+        function ($scope, $http) {
             $scope.title = "Home";
+            $scope.newPost = {};
+
+            $scope.addPost = function () {
+                $http.post($scope.api + "/posts", $scope.newPost)
+                    .then(function (response) {
+                        $scope.posts.push($scope.newPost);
+                        $scope.newPost = {};
+                    });
+            }
         }
     ]);
--- Scripts/Controllers/main.controller.js
+++ Scripts/Controllers/main.controller.js
@@ -3,10 +3,18 @@
 angular.module("mainModule")
     .controller("MainController", [
         "$scope",
-        "$route",
         "$location",
-        function ($scope, $route, $location) {
+        "$route",
+        "repository",
+        function ($scope, $route, $location, repository) {
             $scope.$route = $route;
+            $scope.api = "http://dummyapi.kodalagom.se/api";
+            $scope.posts = {};
+
+            repository.getPosts($scope.api)
+                .then(function (data) {
+                    $scope.posts = data;
+                })
 
             $scope.go = function (url) {
                 $location.path(url);
